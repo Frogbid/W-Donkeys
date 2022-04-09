1cce1bf921c74ad78d9449aed9a3b080c6058acc
Mugdho <saha2171515@stud.kuet.ac.bd>
2022-04-09 13:01:40 +0600

    second removed and tagline changes

--- resources/js/timer.js
+++ resources/js/timer.js
@@ -27,8 +27,7 @@
                 distance = countDown - now;
             document.getElementById("days").innerText = Math.floor(distance / (day));
                 document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute));
 
             //do something later when date is reached
 
